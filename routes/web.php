7131684ef6698878a92989f26c4d432d466bc634
Jwfelinto <jwfelinto@gmail.com>
2025-11-04 01:05:54 -0300

    Commit: feat(groups): add group crud routes.

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,22 @@
 <?php
+# routes/web.php
 
+use App\Livewire\Actions\Groups\GroupCreate;
+use App\Livewire\Actions\Groups\GroupDelete;
+use App\Livewire\Actions\Groups\GroupEdit;
+use App\Livewire\Actions\Groups\GroupIndex;
+use App\Livewire\Actions\Groups\GroupShow;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
-require __DIR__.'/auth.php';
+Route::middleware(['auth'])->group(function () {
+    /**
+     * Groups
+     */
+    Route::prefix('/groups')->name('groups.')->group(function () {
+        Route::get('/', GroupIndex::class)->name('index');
+        Route::get('/criar', GroupCreate::class)->name('create');
+        Route::get('/{group}', GroupShow::class)->name('show');
+        Route::get('/{group}/editar', GroupEdit::class)->name('edit');
+        Route::get('/{group}/deletar', GroupDelete::class)->name('delete');
+    });
+});
