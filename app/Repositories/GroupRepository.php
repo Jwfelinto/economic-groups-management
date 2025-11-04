7dc1d2013e6dbdd43fde0ea15ab0374dc8d25145
Jwfelinto <jwfelinto@gmail.com>
2025-11-04 00:46:14 -0300

    feat(groups): create GroupRepository e GroupRepositoryInterface with methods 'getAll', 'showWithBrands', 'save' and 'delete'.

---
+++ app/Repositories/GroupRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Group;
+use App\Repositories\Interfaces\GroupRepositoryInterface;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class GroupRepository implements GroupRepositoryInterface
+{
+    public function __construct(private readonly Group $group)
+    {
+    }
+
+    public function getAll(array $filters = []): LengthAwarePaginator
+    {
+        return $this->group
+            ->select(['id', 'name', 'created_at'])
+            ->withCount('brands')
+            ->when(! empty($filters['search']), fn ($q) => $q->where('name', 'like', "% {$filters['search']} %"))
+            ->orderBy('name')
+            ->paginate(10);
+    }
+
+    public function showWithBrands(Group $group): ?Group
+    {
+        return $group->with(['brands' => fn ($q) => $q->select('id', 'name', 'group_id')])->findOrFail($id);
+    }
+
+    public function save(Group $group): Group
+    {
+        $group->save();
+
+        return $group;
+    }
+
+    public function delete(Group $group): void
+    {
+        $group->delete();
+    }
+}
