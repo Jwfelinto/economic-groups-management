f166d3a3b488e04ae76403db183de61cb9b9e6e9
Jwfelinto <jwfelinto@gmail.com>
2025-11-04 02:00:20 -0300

    Commit: feat(brands): add brand crud routes.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 # routes/web.php
 
+use App\Livewire\Actions\Brands\BrandCreate;
+use App\Livewire\Actions\Brands\BrandDelete;
+use App\Livewire\Actions\Brands\BrandEdit;
+use App\Livewire\Actions\Brands\BrandIndex;
+use App\Livewire\Actions\Brands\BrandShow;
 use App\Livewire\Actions\Groups\GroupCreate;
 use App\Livewire\Actions\Groups\GroupDelete;
 use App\Livewire\Actions\Groups\GroupEdit;
@@ -18,5 +23,17 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/{group}', GroupShow::class)->name('show');
         Route::get('/{group}/editar', GroupEdit::class)->name('edit');
         Route::get('/{group}/deletar', GroupDelete::class)->name('delete');
+    });
+
+
+    /**
+     * Brands
+     */
+    Route::prefix('/brands')->name('brands.')->group(function () {
+        Route::get('/', BrandIndex::class)->name('index');
+        Route::get('/criar', BrandCreate::class)->name('create');
+        Route::get('/{brand}', BrandShow::class)->name('show');
+        Route::get('/{brand}/editar', BrandEdit::class)->name('edit');
+        Route::get('/{brand}/deletar', BrandDelete::class)->name('delete');
     });
 });
