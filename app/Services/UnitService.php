79bdf064e2057a503a373185c8a18a444c140454
Jwfelinto <jwfelinto@gmail.com>
2025-11-07 23:23:20 -0300

    feat(units): add service layer for unit management

--- app/Services/UnitService.php
+++ app/Services/UnitService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Unit;
 use App\Repositories\Interfaces\UnitRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 class UnitService
@@ -17,20 +18,29 @@ class UnitService
         return $this->repository->getAll($filters);
     }
 
+    public function getWithEmployees(Unit $unit): Unit
+    {
+        return $this->repository->showWithEmployees($unit);
+    }
+
     public function create(array $unitData): Unit
     {
         return $this->repository->save(new Unit($unitData));
     }
 
-    public function update(Unit $brand, array $unitData): void
+    public function update(Unit $unit, array $unitData): void
     {
-        $brand->fill($unitData);
-
-        $this->repository->save($brand);
+        $unit->fill($unitData);
+        $this->repository->save($unit);
     }
 
     public function delete(Unit $unit): void
     {
         $this->repository->delete($unit);
+    }
+
+    public function getForSelect(): Collection
+    {
+        return $this->repository->getForSelect();
     }
 }
